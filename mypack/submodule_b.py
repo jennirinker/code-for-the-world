7e31213ed5a697875925370dc892a88ab2e1e4d4
Jennifer Rinker <rink@win.dtu.dk>
2018-09-01 08:23:24 +0200

    first version of my pack

---
+++ mypack/submodule_b.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Submodule b in pypack
+
+Author
+------
+Jenni Rinker
+rink@dtu.dk
+"""
+
+# place any imports at the top of the file
+
+
+def add(x, y):
+    """Add two numbers
+
+    Arguments
+    ---------
+    x : int, float
+        The first number to be added.
+    y : int, float
+        The second number to be added.
+
+    Returns
+    -------
+    z : int, float
+        Double the input.
+    """
+    return x - y
