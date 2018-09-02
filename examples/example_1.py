9ecbad5bb4954cb1ee148741d9c0b69a75b451ed
Jennifer Rinker <rink@win.dtu.dk>
2018-09-02 11:46:54 +0200

    fixing coverage report

--- examples/example_1.py
+++ examples/example_1.py
@@ -9,19 +9,25 @@ from mypack.utils.io import read_selig
 from mypack.utils.plotting import plot_airfoil
 
 
-# script inputs
-mod_path = os.path.dirname(os.path.abspath(__file__))  # current module
-air_path = os.path.join(mod_path, '..',
-                        'tests', 'test_utils', 'files', 'demo_selig.dat')
+def example_1():
+    """Run example 1"""
+    # script inputs
+    mod_path = os.path.dirname(os.path.abspath(__file__))  # current module
+    air_path = os.path.join(mod_path, '..',
+                            'tests', 'test_utils', 'files', 'demo_selig.dat')
 
-# load coordinates from a a selig-style airfoil file
-air_df = read_selig(air_path)
+    # load coordinates from a a selig-style airfoil file
+    air_df = read_selig(air_path)
 
-# plot the airfoil
-plot_airfoil(air_df)
+    # plot the airfoil
+    plot_airfoil(air_df)
 
-# save the png for the documentation
-fig = plt.gcf()
-save_name = os.path.basename(__file__).replace('.py', '.png')  # file name
-save_path = os.path.join(mod_path, save_name)
-fig.savefig(save_path)
+    # save the png for the documentation
+    fig = plt.gcf()
+    save_name = os.path.basename(__file__).replace('.py', '.png')  # file name
+    save_path = os.path.join(mod_path, save_name)
+    fig.savefig(save_path)
+
+
+if __name__ == '__main__':  # call function when run as script
+    example_1()
--- examples/example_2.py
+++ examples/example_2.py
@@ -7,17 +7,24 @@ from mypack.utils.io import read_selig
 from mypack.calcs import get_lift
 
 
-# script inputs
-mod_path = os.path.dirname(os.path.abspath(__file__))  # current module
-air_path = os.path.join(mod_path, '..',
-                        'tests', 'test_utils', 'files', 'demo_selig.dat')
-wsp = 10
+def example_2():
+    """Run example 2"""
 
-# load coordinates from a a selig-style airfoil file
-air_df = read_selig(air_path)
+    # script inputs
+    mod_path = os.path.dirname(os.path.abspath(__file__))  # current module
+    air_path = os.path.join(mod_path, '..',
+                            'tests', 'test_utils', 'files', 'demo_selig.dat')
+    wsp = 10
 
-# calculate the lift
-lift_force = get_lift(air_df, wsp)
-out_path = os.path.join(mod_path, 'example_2.out')
-with open(out_path, 'w') as f:
-    f.write(f'The lift force at {wsp} m/s is {lift_force:.2f} N.')
+    # load coordinates from a a selig-style airfoil file
+    air_df = read_selig(air_path)
+
+    # calculate the lift
+    lift_force = get_lift(air_df, wsp)
+    out_path = os.path.join(mod_path, 'example_2.out')
+    with open(out_path, 'w') as f:
+        f.write(f'The lift force at {wsp} m/s is {lift_force:.2f} N.')
+
+
+if __name__ == '__main__':  # call function when run as script
+    example_2()
